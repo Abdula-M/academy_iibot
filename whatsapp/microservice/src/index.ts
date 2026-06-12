6e322e14e59c7c7f35ff40e51f8eb353ba783e5f
Abdula-M <Medjidov-A-T@yandex.ru>
2026-06-12 14:42:43 +0300

    fix: update base image to bookworm for newer chromium and add webVersionCache to fix hanging whatsapp initialization

--- whatsapp/microservice/src/index.ts
+++ whatsapp/microservice/src/index.ts
@@ -51,6 +51,10 @@ async function startWhatsApp() {
         puppeteer: {
             executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || '/usr/bin/chromium',
             args: puppeteerArgs
+        },
+        webVersionCache: {
+            type: 'remote',
+            remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html',
         }
     });
 
